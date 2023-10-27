c5ca2e195cdceeb538c3564b88d0d781723f1d94
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2023-10-27 19:12:09 +0300

    HW #54_2 annotation-introduction

---
+++ src/main/java/telran/test/TestRunner.java
@@ -0,0 +1,86 @@
+package telran.test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import telran.test.annotation.BeforeEach;
+import telran.test.annotation.Test;
+
+public class TestRunner implements Runnable {
+    private Object testObj;
+    
+    public TestRunner(Object testObj) {
+        super();
+        this.testObj = testObj;
+    }
+
+    @Override
+    public void run() {
+        Class<?> clazz = testObj.getClass();
+        Method[] methods = clazz.getDeclaredMethods();
+        
+        Method beforeEachMethod = null;
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(BeforeEach.class)) {
+                beforeEachMethod = method;
+//                beforeEachMethod.setAccessible(true);
+                break;
+            }
+        }
+
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(Test.class)) {
+                if (beforeEachMethod != null) {
+                    try {
+                        beforeEachMethod.invoke(testObj);
+                    } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+                        System.out.println("error in BeforeEach: " + e.getMessage());
+                    }
+                }
+                method.setAccessible(true);
+                try {
+                    method.invoke(testObj);
+                } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+                    System.out.println("error: " + e.getMessage());
+                }
+            }
+        }
+    }
+}
+
+
+
+
+//package telran.test;
+//
+//import java.lang.reflect.InvocationTargetException;
+//import java.lang.reflect.Method;
+//
+//import telran.test.annotation.Test;
+//
+//public class TestRunner implements Runnable {
+//	private Object testObj;
+//	
+//	public TestRunner(Object testObj) {
+//		super();
+//		this.testObj = testObj;
+//	}
+//
+//	@Override
+//	public void run() {
+//			Class<?> clazz = testObj.getClass();
+//			Method[] methods = clazz.getDeclaredMethods();
+//			for(Method method: methods) {
+//				if(method.isAnnotationPresent(Test.class)) {
+//					method.setAccessible(true);
+//					try {
+//						method.invoke(testObj);
+//					} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+//						System.out.println("error: " + e.getMessage());
+//					}
+//				}
+//			}
+//
+//	}
+//
+//}
